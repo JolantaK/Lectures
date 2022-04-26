1535e916327f7f5bf00fce76544a87308e4beffa
JolantaK <jolanta_apele@inbox.lv>
2022-04-26 21:53:31 +0300

    Added class

---
+++ Lectures3/Lectures3/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Lectures3
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+            Console.WriteLine($"Creating person with name: {name}");
+        }
+
+        public Person()
+        {
+            Name = "Unknwon";
+            Console.WriteLine("Creating Jone Doe");
+        }
+
+        public string Name { get; }
+
+        public int Age { get; set; }
+
+        public bool IsMale { get; set; }
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Hello my name is {Name} and my age is {Age}");
+        }
+
+        public void Call(string phoneNumber)
+        {
+            Console.WriteLine($"{Name} is calling to {phoneNumber}");
+        }
+    }
+}
---
+++ Lectures3/Lectures3/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using Lectures3;
+
+Person artis = new Person("Artis");
+
+artis.Age = 55;
+artis.IsMale = true;
+
+string artisName = artis.Name;
+Console.WriteLine($"I have created with name: {artisName}");
+
+artis.Greeting();
+artis.Call("2222222222");
+
+
+Person person = new Person();
+
+person.Age = 14;
+person.IsMale = false;
+
+person.Greeting();
+person.Call("3333333333333");
+
+
+
+Console.WriteLine();
+Console.WriteLine("=================================");
+Console.WriteLine();
+
+
+
+string name = "Artis";
+string nameToChange = name;
+nameToChange = "11111111";
+
+Console.WriteLine($"Name is: {name}");
+Console.WriteLine($"Name to change is: {nameToChange}");
+
+
+Person person2 = new Person("Artis");
+person2.Age = 40;
+
+Person personToChange = person2;
+personToChange.Age = 55;
+
+Console.WriteLine($"Person age: {person2.Age}");
+Console.WriteLine($"Person to change age: {personToChange.Age}");
